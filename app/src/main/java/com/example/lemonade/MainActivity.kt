2c202e994fad17776c03d13e5267dfa4311bdaeb
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-11-30 23:40:25 +0530

    Configuring the ImageView
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -73,11 +73,12 @@ class MainActivity : AppCompatActivity() {
         lemonImage = findViewById(R.id.image_lemon_state)
         setViewElements()
         lemonImage!!.setOnClickListener {
-            // TODO: call the method that handles the state when the image is clicked
+            // Calling the method that handles the state when the image is clicked
+            clickLemonImage()
         }
         lemonImage!!.setOnLongClickListener {
-            // TODO: replace 'false' with a call to the function that shows the squeeze count
-            false
+            // Calling  the function that shows the squeeze count
+            showSnackbar()
         }
     }
 
